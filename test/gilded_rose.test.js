f9a48ba0982855bf5c9477bf261b6d2b824d195b
Pablo Albaladejo <pablo.albaladejo@guidesmiths.com>
2019-11-29 16:16:16 +0100

    quality never more than 50

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -28,7 +28,24 @@ describe("Gilded Rose", function () {
     expect(items[0].name).toBe(name);
     expect(items[0].sellIn).toBe(sellIn-1);
     expect(items[0].quality).toBe(quality-1);
+    expect(items[0].quality).toBeLessThanOrEqual(50);
   });
+
+  it("should work with quality > 0", function () {
+
+    const name = "foo";
+    const sellIn = 0;
+    const quality = 1;
+
+    const gildedRose = new Shop([new Item(name, sellIn, quality)]);
+    const items = gildedRose.updateQuality();
+  
+    expect(items[0].name).toBe(name);
+    expect(items[0].sellIn).toBe(sellIn-1);
+    expect(items[0].quality).toBe(quality-1);
+    expect(items[0].quality).toBeLessThanOrEqual(50);
+  });
+
 
 
 });
