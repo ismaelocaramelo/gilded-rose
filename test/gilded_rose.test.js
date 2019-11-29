8e3acebce6fd6f5c499038a727d2924ef4c8a950
Pablo Albaladejo <pablo.albaladejo@guidesmiths.com>
2019-11-29 16:12:56 +0100

    base case quality > 0

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -1,6 +1,7 @@
 const { Shop, Item } = require("../src/gilded_rose");
 
 describe("Gilded Rose", function () {
+  
   it("should work with base case", function () {
 
     const name = "foo";
@@ -14,4 +15,20 @@ describe("Gilded Rose", function () {
     expect(items[0].sellIn).toBe(sellIn-1);
     expect(items[0].quality).toBe(quality);
   });
+
+  it("should work with quality > 0", function () {
+
+    const name = "foo";
+    const sellIn = 0;
+    const quality = 1;
+
+    const gildedRose = new Shop([new Item(name, sellIn, quality)]);
+    const items = gildedRose.updateQuality();
+  
+    expect(items[0].name).toBe(name);
+    expect(items[0].sellIn).toBe(sellIn-1);
+    expect(items[0].quality).toBe(quality-1);
+  });
+
+
 });
